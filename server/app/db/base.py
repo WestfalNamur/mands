4f8599a0ee398105887fe8b08080f5a460a0486a
Stefan Crummenerl <crummenerls@gmail.com>
2022-02-26 15:40:37 +0100

    [server:TST] Improve base test for db for better understanding.

--- server/app/db/base.py
+++ server/app/db/base.py
@@ -5,6 +5,17 @@
     FastAPI: https://fastapi.tiangolo.com/advanced/async-sql-databases/?h=sql
 """
 
+from databases import Database
 
 DB_SOURCE = "postgresql://mands_user:mands_pw@localhost:5432/mands_db?sslmode=disable"
+
+
+async def create_db(db_source_url: str, testing=False) -> Database:
+    """Create a db conneciton."""
+    if testing:
+        database = Database(db_source_url, force_rollback=True)
+    else:
+        database = Database(db_source_url, force_rollback=False)
+    await database.connect()
+    return database
 
