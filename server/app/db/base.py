cf9a3b4cbc4c9802593a51f7b60218d7b5e193d7
Stefan Crummenerl <crummenerls@gmail.com>
2022-03-04 17:17:13 +0100

    [server:ENH] Finish CRUD functions for /users

--- server/app/db/base.py
+++ server/app/db/base.py
@@ -4,15 +4,11 @@ import databases
 
 mandsenv = os.environ["MANDSENV"]
 
-# Database
 DATABASE_URL = (
     "postgresql+asyncpg://mands_user:mands_pw@localhost:5432/mands_db?sslmode=disable"
 )
-if mandsenv == "testing":
-    database = databases.Database(DATABASE_URL, force_rollback=True)
-elif mandsenv == "dev":
-    database = databases.Database(DATABASE_URL, force_rollback=False)
-elif mandsenv == "production":
+
+if mandsenv in ["dev", "production"]:
     database = databases.Database(DATABASE_URL, force_rollback=False)
 else:
     database = databases.Database(DATABASE_URL, force_rollback=True)
