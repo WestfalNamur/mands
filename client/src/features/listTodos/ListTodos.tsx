08ddaccc332e8ab6b32acd93ceb851749e938884
Stefan Crummenerl <crummenerls@gmail.com>
2022-03-08 21:53:40 +0100

    [server:REF] Cleanup react-query example

--- client/src/features/listTodos/ListTodos.tsx
+++ client/src/features/listTodos/ListTodos.tsx
@@ -6,15 +6,14 @@ import styles from "./ListTodos.module.css";
 import {useQuery} from "react-query";
 
 export default function Index() {
-  const {isLoading, error, data} = useQuery<Todo[], Error>('repoData', () =>
+  const {isLoading, error, data} = useQuery<Todo[], Error>('todos', () =>
     fetch('http://localhost:8000/todos?limit=10&offset=0')
       .then(res => res.json())
   )
 
   if (isLoading) return <p>...loading.</p>
-  if (error) return <p>{error}</p>;
-  if (!data) return <p>no data ...</p>;
-  console.log(data)
+  if (error) return <p>{error}</p>
+  if (!data) return <p>no data ...</p>
 
   const todos: Todo[] = data.filter((todo: Todo) => isTodo(todo))
   const items = todos.map(todo => <ListTodosItem key={todo.id} {...todo}/>)
